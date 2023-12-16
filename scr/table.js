0d8c83ca6976e940cec726f164812d2cd58613ce
Elizabeth Malfoy <elmalfoy73@gmail.com>
2023-12-16 14:59:44 +0300

    fix: lab6, preloader

--- scr/table.js
+++ scr/table.js
@@ -21,11 +21,13 @@ form.addEventListener('submit', async function(event) {
     localStorage.setItem('priceFrom', priceFrom);
     localStorage.setItem('priceTo', priceTo);
     localStorage.setItem('availability', availability);
-
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'block';
     // Получение данных о книгах с сервера
     const response = await fetch('https://retoolapi.dev/fW96ZD/books/');
     const books = await response.json(); // Парсинг JSON данных о книгах
     const table = generateTable(books, genre, author, Number(year), Number(priceFrom), Number(priceTo), availability);
+    preloader.style.display = 'none';
 
     // Очистка контейнера результатов и добавление сгенерированной таблицы
     resultsTable.innerHTML = '';
